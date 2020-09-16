d9a9cab1562fbc897964d72bdd4edb6310e05c45
rodrigo <contacto.lunitas@gmail.com>
2020-09-16 20:07:29 -0300

    Todo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebFinal2.Models;
+
+namespace WebFinal2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                using (ApplicationDbContext db = new ApplicationDbContext())
+                {
+                    var idUsuarioActual = User.Identity.GetUserId();
+
+                    var roleManager = new RoleManager<IdentityRole>
+                        (new RoleStore<IdentityRole>(db));
+
+
+                    //CREAR ROLE
+
+                    //var resultado = roleManager.Create(new IdentityRole("ApruebaAcciones"));
+                    var resultado2 = roleManager.Create(new IdentityRole("AccederPagina"));
+
+
+                    var userManager = new UserManager<ApplicationUser>
+                         (new UserStore<ApplicationUser>(db));
+
+                    //AGREGAR AL USUARIO AL ROL
+
+
+                    //resultado = userManager.AddToRole(idUsuarioActual, "ApruebaAcciones");
+                    resultado2 = userManager.AddToRole(idUsuarioActual, "AccederPagina");
+
+                    //USUARIO ESTA EN EL ROL?
+
+                    var usuarioEstaEnRol = userManager.IsInRole(idUsuarioActual, "ApruebaAcciones");
+                    var usuarioEstaEnRol2 = userManager.IsInRole(idUsuarioActual, "AccederPagina");
+
+
+                    //Roles Usuario
+
+                    var roles = userManager.GetRoles(idUsuarioActual);
+
+
+                    //Remover Usuario de Rol
+
+                    //resultado = userManager.RemoveFromRole(idUsuarioActual, "ApruebaAcciones");
+
+
+                    //Borrar ROL
+
+                    //var rolVendedor = roleManager.FindByName("ApruebaAcciones");
+                    //roleManager.Delete(rolVendedor);
+
+
+
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/TablaController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebFinal2.Models;
+using WebFinal2.Models.ViewModels;
+
+namespace WebFinal2.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        [Authorize]
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+
+            using (CrudEntities db = new CrudEntities())
+            {
+
+
+
+                lst = (from d in db.operacion
+                       orderby d.tipo ascending
+
+
+
+                       select new ListTablaViewModel
+                       {
+                           id_operacion = d.id_operacion,
+                           concepto = d.concepto,
+                           monto = d.monto,
+                           tipo=d.tipo,
+                           
+
+
+                       }).ToList();
+
+
+            }
+
+
+            return View(lst);
+        }
+
+
+        [Authorize]
+        public ActionResult Nuevo()
+        {
+
+            return View();
+
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    using (CrudEntities db = new CrudEntities())
+                    {
+                        var oTabla = new operacion();
+
+                        oTabla.concepto = model.concepto;
+                        oTabla.monto = model.monto;
+                        oTabla.fecha_op = model.fecha_op;
+                        oTabla.tipo = model.tipo;
+
+                        db.operacion.Add(oTabla);
+                        db.SaveChanges();
+
+                    }
+
+                    return Redirect("~/Tabla/");
+
+                }
+
+                return View(model);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+
+
+
+
+        }
+
+        [Authorize]
+        public ActionResult Editar(int Id)
+        {
+
+            TablaViewModel model = new TablaViewModel();
+
+            using (CrudEntities db = new CrudEntities())
+            {
+                var oTabla = db.operacion.Find(Id);
+                model.concepto = oTabla.concepto;
+                model.monto = oTabla.monto;
+               
+                model.id_operacion = oTabla.id_operacion;
+            }
+
+
+            return View(model);
+
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Editar(TablaViewModel model)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    using (CrudEntities db = new CrudEntities())
+                    {
+                        var oTabla = db.operacion.Find(model.id_operacion);
+
+                        oTabla.concepto = model.concepto;
+                        oTabla.monto = model.monto;
+                        oTabla.fecha_op = model.fecha_op;
+
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+
+                    }
+
+                    return Redirect("~/Tabla/");
+
+                }
+
+                return View(model);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+
+        }
+
+
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Eliminar(int Id)
+        {
+
+
+
+            using (CrudEntities db = new CrudEntities())
+            {
+
+
+                var oTabla = db.operacion.Find(Id);
+                db.operacion.Remove(oTabla);
+                db.SaveChanges();
+            }
+
+
+            return Redirect("~/Tabla/");
+        }
+
+
+
+
+
+
+    }
+
+}
